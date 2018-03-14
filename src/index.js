591454d806a4d4eb5139ec31b76fca8507a8cf75
José Oliveira <ZPaulo@users.noreply.github.com>
2018-03-14 18:41:53 +0000

    Updating readme

--- src/index.js
+++ src/index.js
@@ -3,5 +3,7 @@ import "bootstrap/dist/css/bootstrap-theme.css";
 import React from "react";
 import { render } from "react-dom";
 import App from "./App";
+import registerServiceWorker from "./registerServiceWorker";
 
 render(React.createElement(App, null, null), document.getElementById("root"));
+registerServiceWorker();
